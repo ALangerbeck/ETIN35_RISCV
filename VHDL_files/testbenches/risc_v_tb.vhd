1cb7a1074126b79a248a00592e979a1fd3546a0f
Tinnis97 <lina.tinnerberg@gmail.com>
2022-05-03 10:27:06 +0200

    Create risc_v_tb.vhd

---
+++ VHDL_files/testbenches/risc_v_tb.vhd
@@ -0,0 +1,22 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.std_logic_textio.all;
+use ieee.numeric_std.all;
+use STD.textio.all;
+
+entity risc_v_tb is
+end entity;
+
+
+architecture structural of  risc_v_tb is
+
+
+	--Signal Declaration
+
+	--Component declaration
+
+	
+begin
+  
+
+end structural;
