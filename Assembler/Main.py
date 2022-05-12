8e39af02020eb57880ee119be8bf059f5c021e6a
Alfred Langerbeck <32991448+ALangerbeck@users.noreply.github.com>
2022-05-12 14:51:36 +0200

    More on assembler

--- Assembler/Main.py
+++ Assembler/Main.py
@@ -1,5 +1,9 @@
-import sys                                          
-import risc_v_32_constatns
+import opcode
+from os import remove
+import sys
+from tabnanny import check
+from risc_v_32_constants import memonicToFunct                                          
+import risc_v_32_constants as constants
 
 lines, lineinfo, lineadr, labels = [], [], [], {}
 LINEINFO_NONE, LINEINFO_ORG, LINEINFO_BEGIN, LINEINFO_END	= 0x00000, 0x10000, 0x20000, 0x40000
@@ -45,19 +49,40 @@ for i in range(len(lines)):                         # PASS 1: do PER LINE replac
         for el in s[2:]: rest += " " + el
         lines[i] = (lines[i][0:k] + rest).strip()   # join everything before and after the #org ... statement
     """
+
     if lines[i].find(':') != -1:
         labels[lines[i][:lines[i].find(':')]] = i   # put label with it's line number into dictionary
         lines[i] = lines[i][lines[i].find(':')+1:]  # cut out the label
 
     lines[i] = lines[i].split()                     # now split line into list of bytes (omitting whitepaces)
 
+    lineinfo.append(lines[i][0])
+
     for j in range(len(lines[i])-1, -1, -1):        # iterate from back to front while inserting stuff
-        try: lines[i][j] = opCodes[lines[i][j]]     # try replacing mnemonic with opcode
+        try: 
+            lines[i][j] = constants.opcodes[lines[i][j]]     # try replacing mnemonic with opcode
         except: 
             if lines[i][j].find('0x') == 0 and len(lines[i][j]) > 4:    # replace '0xWORD' with 'LSB MSB'
                 val = int(lines[i][j], 16)
                 lines[i][j] = str(val & 0xff)
                 lines[i].insert(j+1, str((val>>8) & 0xff))
+
+for i in range(len(lines)):
+    opcode = lines[i][0]
+    ### integer instruction ###
+    if opcode == '0010011':
+        if(len(lines) == 4 ):
+            print("Expecting 3 arguments")
+            break
+        lines[i][1] = "{:05b}".format(int(lines[i][1]))
+        funct = constants.memonicToFunct[lineinfo[i]]
+        lines[i].insert(2,funct)
+        lines[i][3] = "{:05b}".format(int(lines[i][3]))
+        lines[i][4] = "{:012b}".format(int(lines[i][4]))
+    
+    lines[i].reverse()
+    for
+        
 
 
 print(lines)
