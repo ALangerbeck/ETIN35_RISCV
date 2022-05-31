2b9f72329963e2bc59af882f3447ba55a262afcb
Tinnis97 <lina.tinnerberg@gmail.com>
2022-05-31 11:17:14 +0200

    Update Stage_ID.vhd

--- VHDL_files/cpu_stages/Stage_ID.vhd
+++ VHDL_files/cpu_stages/Stage_ID.vhd
@@ -154,8 +154,8 @@ begin
             opcode_c <= "0110011";
             rd_c <= instruction_in(9 downto 7) + 8;
             funct3_c <= "000";
-            rs1_c <= instruction_in(11 downto 7);
-            rs2_c <= instruction_in(6 downto 2);
+            rs1_c <= instruction_in(9 downto 7) + 8;
+            rs2_c <= instruction_in(4 downto 2) + 8;
             funct7_c <= "0100000";
             --c.addi -- for this one rd and rs1 is not allowed to be zero, sbould we program in that in vivado?
         elsif(instruction_in(1 downto 0) = "01" and instruction_in(15 downto 13) = "000") then 
